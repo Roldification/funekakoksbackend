409037563266ec6a606515482fcd25a04fa21c4b
harold <roldify27@gmail.com>
2019-03-22 16:39:40 +0800

    created API for item purchasing config in SC Create

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -6,6 +6,7 @@ use App\SystemUser;
 use Illuminate\Http\Request;
 use App\AccessTokens;
 use App\FisDeceased;
+use App\ServiceContract;
 
 
 class AccessController extends Controller
@@ -40,6 +41,105 @@ class AccessController extends Controller
 				'message'=>$e->getMessage()
 			];	
 		}
+	}
+	
+	public function getMinimalProbabilities(Request $request)
+	{
+		try {
+			$value = (array)json_decode($request->post()['items']);
+			$branch = $request->post()['branch'];
+			$itemSelection = [];
+			$itemPresentation = [];
+			
+			foreach ($value as $row)
+			{
+				$selection = DB::select(DB::raw("select fk_item_id, serialno from
+						_fis_productlist where isEncumbered=1 and branch='".$branch."'
+						and fk_item_id='".$row->item_code."'"));
+				
+				array_push($itemSelection, $selection);
+				
+				$presentation = DB::select(DB::raw("select top ".$row->quantity." item_code, item_name, serialno from _fis_productlist pl
+					inner join _fis_items i on pl.fk_item_id = i.item_code
+					where isEncumbered=1 and branch='".$branch."'and fk_item_id='".$row->item_code."'
+					order by id"));
+				
+				array_push($itemPresentation, $presentation);
+				
+				
+			}
+			
+			return [
+					'selection' => $itemSelection,
+					'presentation' => $itemPresentation,
+					'vals' => $value
+			];
+			
+		} catch (\Exception $e) {
+			
+			return [
+					'status'=>'error',
+					'message'=>$e->getMessage()
+			];
+			
+		}
+	}
+	
+	
+	public function insertContract(Request $request)
+	{
+		try {
+			
+			$value = (array)json_decode($request->post()['servicecontract']);
+			
+			$value['contract_balance'] = $value['contract_amount'];
+			$value['burial_time'] = date('Y-m-d H:i:s', strtotime($value['burial_time']));
+			$serviceContract = ServiceContract::create($value);
+			
+			$user_check = DB::select(DB::raw("select * from
+				(
+				SELECT item_code, item_name, isnull(quantity, 0) as quantity, 0 as price, 0 as discount, 0 as tot_price FROM _fis_items fi
+				left join 
+				(
+				select * from _fis_package_inclusions
+				where fk_package_id=".$serviceContract->package_class_id."
+				and inclusionType='ITEM'
+				)b on fi.item_code = b.item_id
+				)sdf
+				order by quantity desc,  item_code asc
+				"));
+			
+			    $sc_details = DB::select(DB::raw("select sc.contract_id, contract_no, contract_date, (s.lname + ', ' + s.fname + ' ' + s.mname)signee,
+					s.address as signeeaddress, (d.lastname + ', ' + d.firstname + ' ' + d.middlename)deceased, dbo._ComputeAge(d.birthday, getdate())deceasedage,
+					d.birthday, d.address, d.causeOfDeath, sc.mort_viewing, cr.ReligionName
+					from _fis_service_contract sc 
+					inner join _fis_signee s on sc.signee = s.id
+					inner join _fis_deceased d on sc.deceased_id = d.id
+					inner join _fis_package p on sc.package_class_id = p.id
+					inner join ClientReligion cr on d.religion = cr.ReligionID
+					where contract_id=".$serviceContract->id)); 
+							
+			
+			return [
+					'status'=>'saved',
+					'message'=> [
+							'service_contract' => $sc_details,
+							'item_inclusions' => $user_check
+					]
+			];
+			
+
+		} catch (\Exception $e) {
+			
+			return [
+					'status'=>'unsaved',
+					'message'=>$e->getMessage()
+			];
+			
+		}
+		
+		
+		
 	}
 	
 	public function insertAccess(Request $request)
